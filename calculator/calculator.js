fc84ffc1fcbc137f60ef35451cf173570d07eebc
Jackson Gerlach <gerlach.jackson9@gmail.com>
2022-08-02 17:11:01 -0700

    finished most of js, need to fix, css screen with very big numbers, add keyboard func

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -2,34 +2,87 @@ let firstNum = "";
 let lasNum = "";
 let Op = "";
 let doubleOp = false;
+let dotCheck = false;
 
-
+const backButton = document.getElementById('delete');
+const clearButton = document.getElementById('clear');
 const inputScreen = document.getElementById("current-input");
 const currentOp = document.getElementById("display");
 const numbers = document.querySelectorAll('.num');
 const Operation = document.querySelectorAll('.operator');
 const equalbutton = document.getElementById("equal");
 
-
-//getting first num and writing to input screen
 numbers.forEach((element) => {
     element.addEventListener('click', () =>{
-        console.log(firstNum);
-        if(Op === "" && firstNum === ""){
-            firstNum = firstNum.concat(element.innerHTML);
-            inputScreen.innerText = firstNum;
+        if(inputScreen.innerHTML != 'ERROR'){
+            if(Op === ""){
+                if (element.innerHTML != "."){
+                    firstNum = firstNum.concat(element.innerHTML);
+                    inputScreen.innerText = firstNum;
+                }
+                else{
+                    if (dotCheck == false){
+                        firstNum = firstNum.concat(element.innerHTML);
+                        inputScreen.innerText = firstNum;
+                        dotCheck = true;
+                    }
+                }
+            }
+            else{
+                if (element.innerHTML != "."){
+                    lasNum = lasNum.concat(element.innerHTML);
+                    inputScreen.innerText = lasNum
+                }
+                else{
+                    if (dotCheck == false){
+                        lasNum = lasNum.concat(element.innerHTML);
+                        inputScreen.innerText = lasNum
+                        dotCheck = true;
+                    }
+                }
+                
+            }
         }
         else{
-            lasNum = lasNum.concat(element.innerHTML);
-            inputScreen.innerText = lasNum
+            clear();
+            if(Op === ""){
+                if (element.innerHTML != "."){
+                    firstNum = firstNum.concat(element.innerHTML);
+                    inputScreen.innerText = firstNum;
+                }
+                else{
+                    if (dotCheck == false){
+                        firstNum = firstNum.concat(element.innerHTML);
+                        inputScreen.innerText = firstNum;
+                        dotCheck = true;
+                    }
+                }
+            }
+            else{
+                if (element.innerHTML != "."){
+                    lasNum = lasNum.concat(element.innerHTML);
+                    inputScreen.innerText = lasNum
+                }
+                else{
+                    if (dotCheck == false){
+                        lasNum = lasNum.concat(element.innerHTML);
+                        inputScreen.innerText = lasNum
+                        dotCheck = true;
+                    }
+                }
+                
+            }
         }
-
     });
 
 });
 
 Operation.forEach((element) => {
     element.addEventListener('click', () => {
+        dotCheck = false;
+        if(inputScreen.innerHTML == "ERROR"){
+            clear();
+        }
         if (doubleOp == false){
             Op = element.innerHTML;
             display.innerHTML = `${firstNum} ${Op}`;
@@ -37,7 +90,11 @@ Operation.forEach((element) => {
             doubleOp = true;
         }
         else{
+            let tempOp = element.innerHTML;
             operation();
+            console.log(tempOp);
+            Op = tempOp;
+            
         }
 
     });
@@ -45,18 +102,51 @@ Operation.forEach((element) => {
 });
 
 equalbutton.addEventListener("click", () =>{
-    operation();
+    if(Op != "" || lasNum != ""){
+        operation();
+        doubleOp = false;
+    }
+    
+});
+
+clearButton.addEventListener("click", () =>{
+    clear();
+});
+
+backButton.addEventListener("click", () =>{
+    firstNum = inputScreen.textContent.slice(0,[inputScreen.textContent.length-1]);
+    inputScreen.textContent = firstNum;
 });
 
 
+function clear(){
+    firstNum ="";
+    lasNum = "";
+    Op = "";
+    doubleOp = false;
+    inputScreen.innerText = "";
+    display.innerText = "";
+}
+
+
 function operation(){
+    if (lasNum ===""){
+        lasNum = "0";
+    }
+    if (firstNum === ""){
+        firstNum = '0'
+    }
     temp = `${firstNum} ${Op} ${lasNum} =`;
     display.innerText= temp;
     if (Op === "÷"){
+        if(firstNum == 0 || lasNum == 0){
+            inputScreen.innerText= "ERROR";
+            return;
+        }
         firstNum = firstNum/lasNum;
     }
     else if(Op === "+"){
-        firstNum = firstNum + lasNum;
+        firstNum = parseInt(firstNum) + parseInt(lasNum);
     }
     else if(Op === "−"){
         firstNum = firstNum - lasNum;
@@ -68,6 +158,5 @@ function operation(){
     Op = "";
     lasNum ="";
     temp="";
-    doubleOp = false;
 }
 
